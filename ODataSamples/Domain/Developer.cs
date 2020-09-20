4b3f0b76b1a02fc500016b52db63669dd2c57184
adlerpagliarini <adlerpagliarini@gmail.com>
2020-09-20 13:30:44 -0300

    OData Mapping Protected Props

--- ODataSamples/Domain/Developer.cs
+++ ODataSamples/Domain/Developer.cs
@@ -6,39 +6,33 @@ namespace ODataSamples.Domain
 {
     public class Developer : Identity<Developer>
     {
-        public string Name { get; set; }
-        public DevType DevType { get; set; }
-        public Goal Goal { get; set; }
+        public Developer(string name, DevType devType)
+        {
+            Name = name;
+            DevType = devType;
+            _tasksToDo = new Collection<TaskToDo>();
+        }
+
+        public string Name { get; protected set; }
+        public DevType DevType { get; protected set; }
+        public Goal Goal { get; protected set; }
         private ICollection<TaskToDo> _tasksToDo { get; set; }
         public virtual IReadOnlyCollection<TaskToDo> TasksToDo { get { return _tasksToDo as Collection<TaskToDo>; } }
 
-        public Developer()
+        protected Developer()
         {
             _tasksToDo = new Collection<TaskToDo>();
-            Goal = new Goal();
         }
 
         public void AddItemToDo(TaskToDo todo)
         {
-            var _todo = new TaskToDo()
-            {
-                Start = todo.Start,
-                DeadLine = todo.DeadLine,
-                Title = todo.Title,
-                Status = todo.Status,
-                Id = this.Id
-            };
+            var _todo = new TaskToDo(todo.Title, todo.Start, todo.DeadLine, todo.Status, todo.UserId);
             _tasksToDo.Add(_todo);
         }
 
         public void SetGoal(Goal goal)
         {
-            var _goal = new Goal()
-            {
-                Title = goal.Title,
-                UserId = this.Id
-            };
-
+            var _goal = new Goal(goal.Title, goal.UserId);
             Goal = _goal;
         }
     }
